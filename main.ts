7f240e4864bf31b7d07425619da600c444142b55
LudumStudios <65925982+Shiven121S@users.noreply.github.com>
2021-10-06 21:43:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 function SpawnResourcesGold () {
     for (let value of tiles.getTilesByType(assets.tile`myTile0`)) {
-        if (tiles.locationXY(value, tiles.XY.row) >= 8) {
-            tiles.setTileAt(value, assets.tile`myTile2`)
+        if (tiles.locationXY(value, tiles.XY.row) >= 11) {
+            if (Math.percentChance(30)) {
+                tiles.setTileAt(value, assets.tile`myTile2`)
+            }
         }
     }
 }
